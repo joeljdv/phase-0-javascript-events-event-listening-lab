2d28235d88685471ef99b761bea9fd3cb320ba34
joeljdv <joeldiazjdv@gmail.com>
2021-03-23 11:01:04 -0400

    commit tree 3436bae56539f2f1bae63dd31bf8ee577d7c62ea

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const div=document.getElementsByTagName('div')[5]
+    const input=document.getElementById('input')
+    input.addEventListener('click',addingEventListener)
+    div.addEventListener('click',addingEventListener)
+    alert('I was clicked')
 }
+
+addingEventListener()
